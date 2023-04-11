dc26a35b68a4a3d32e432847a116c07a07f1671d
LcyTohka <LCYabc1270634697@outlook.com>
2023-04-11 20:52:40 +0800

    20230411更新

--- GeekBang/TonyBai/lesson30/lesson30.1.go
+++ GeekBang/TonyBai/lesson30/lesson30.1.go
@@ -1,1 +1,27 @@
-package lesson30
+package main
+
+import "fmt"
+
+func add(a []int, i int, j int) []int {
+	b := a[:i]
+	c := []int{}
+	fmt.Println(a[i:])
+	for k := 1; k <= len(a[i:]); k++ {
+		c = append(c, a[i+k-1])
+	}
+	fmt.Println(c)
+	b = append(b, j)
+	b = append(b, c...)
+	return b
+}
+
+//func delete(i int, j int) []int {
+//	return
+//}
+
+func main() {
+	b := []int{1, 2, 3, 5}
+	a := add(b, 3, 4)
+	fmt.Println(a)
+
+}
